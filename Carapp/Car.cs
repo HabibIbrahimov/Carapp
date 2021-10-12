e7741c5ac0d18c25195929f4f3718d235fd4bd35
HabibIbrahimov <habibyi@code.edu.az>
2021-10-12 20:33:27 +0400

    Add project files.

---
+++ Carapp/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carapp
+{
+    class Car
+    {
+        public int Depopetrol { get; set; }
+        public int DepoLimit { get; set; }
+        public double Km { get; set; }
+        public string DriveMod { get; set; }
+        public int Lt { get; set; }
+        
+        
+
+        public Car( int depopetrol,double km,int lt,string drivemod)
+        {
+           Depopetrol = depopetrol;
+           DepoLimit = 70;
+           Km = km;
+           Lt = lt;
+           DriveMod = drivemod;
+        }
+
+        public void Carpetrol()
+        {
+            if (Depopetrol==Km || Depopetrol>=Km)
+            {
+                Depopetrol--;
+                Console.WriteLine("Gede biler");
+            }
+            else
+            {
+                Console.WriteLine("Gede bilmez");
+            }
+        }
+        public void Mod()
+        {
+            if (DriveMod=="Eco")
+            {
+                Console.WriteLine("10lt/100km");
+            }
+            if (DriveMod=="Sport")
+            {
+                Console.WriteLine("13lt/100km");
+            }
+        }
+        public void Fuel()
+        {
+            if (Depopetrol<DepoLimit)
+            {
+                Depopetrol++;
+            }
+            else
+            {
+                Console.WriteLine("Verilen yol gedile bilmez");
+            }
+        }
+        
+        
+        
+        
+    }
+}
---
+++ Carapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Carapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Fuelcar = new Car(40,10,30,"sport");
+            Fuelcar.Carpetrol();
+            Fuelcar.Mod();
+            Fuelcar.Fuel();
+            Console.WriteLine("Yaxsi yol");
+
+        }
+    }
+}
